4b0b28f94360c8176c2dd796b109b3cbecf3c836
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-08-14 18:47:08 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    word_array = word.split("").sort
+    result = []
+    array.each do |item|
+       if item.split("")sort == word_array
+       result << item
+    end
+    result
+  end
+
+end
