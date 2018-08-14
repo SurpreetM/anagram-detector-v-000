72eae07113da4680ac233dc3c9b8884934474415
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-08-14 18:55:15 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,13 +8,18 @@ class Anagram
   end
 
   def match(array)
+    #split the word into an array of letters and sort
     word_array = word.split("").sort
+    #create an empty array for to store the anagrams
     result = []
+    #split each potential anagram into an array and sort, then compare to word array.
+    #if true - add the original anagram to the results array
     array.each do |item|
       if item.split("").sort == word_array
       result << item
      end
-   end
+   end #end loop
+   #return the results array
     result
   end
 
